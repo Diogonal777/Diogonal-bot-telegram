56597ad18660cafd444cf78463580b3dfe2d8a87
Diogonal777 <voitovichvadim0@gmail.com>
2025-04-29 20:03:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,51 @@ const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 
 app.use(bodyParser.json());
 
+// Обработка входящих обновлений от Telegram
 app.post('/webhook', async (req, res) => {
-  const message = req.body.message?.text;
-  const chatId = req.body.message?.chat?.id;
+  const body = req.body;
 
-  if (message === '/start') {
+  // Обработка команды /start
+  if (body.message && body.message.text === '/start') {
+    const chatId = body.message.chat.id;
+
     await axios.post(`${TELEGRAM_API}/sendMessage`, {
       chat_id: chatId,
-      text: 'Привет! Я всегда онлайн!',
+      text: 'Привет! Выберите опцию:',
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: 'Кнопка 1', callback_data: 'button1' }],
+          [{ text: 'Кнопка 2', callback_data: 'button2' }]
+        ]
+      }
+    });
+  }
+
+  // Обработка нажатий на инлайн-кнопки
+  if (body.callback_query) {
+    const chatId = body.callback_query.message.chat.id;
+    const data = body.callback_query.data;
+
+    let responseText = '';
+
+    if (data === 'button1') {
+      responseText = 'Вы нажали кнопку 1!';
+    } else if (data === 'button2') {
+      responseText = 'Вы нажали кнопку 2!';
+    } else {
+      responseText = 'Неизвестная команда.';
+    }
+
+    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+      chat_id: chatId,
+      text: responseText
     });
   }
 
   res.sendStatus(200);
 });
 
+// Проверка работы сервера
 app.get('/', (req, res) => {
   res.send('Бот работает!');
 });
