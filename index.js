1ab7071f79ab6772ab77312be179f07628d848f5
Diogonal777 <voitovichvadim0@gmail.com>
2025-04-29 19:47:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
+const app = express();
+
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+
+app.use(bodyParser.json());
+
+app.post('/webhook', async (req, res) => {
+  const message = req.body.message?.text;
+  const chatId = req.body.message?.chat?.id;
+
+  if (message === '/start') {
+    await axios.post(`${TELEGRAM_API}/sendMessage`, {
+      chat_id: chatId,
+      text: 'Привет! Я всегда онлайн!',
+    });
+  }
+
+  res.sendStatus(200);
+});
+
+app.get('/', (req, res) => {
+  res.send('Бот работает!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
