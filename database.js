91e4203fe079f84c0f292b1b8f674d8b797e6dfb
Diogonal777 <voitovichvadim0@gmail.com>
2025-05-15 23:30:12 +0300

    Update database.js

--- database.js
+++ database.js
@@ -13,18 +13,3 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://YOUR_MONGO_URL", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-const userSchema = new mongoose.Schema({
-    userId: Number,
-    balance: { type: Number, default: 100 }  // Начальный баланс
-});
-
-const User = mongoose.model("User", userSchema);
-
-module.exports = { User };
